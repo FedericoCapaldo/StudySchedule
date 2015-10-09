d0949e99528abca4b62c53ae0a757e86663e18f0
andrei <andreins@ymail.com>
2015-10-09 21:06:44 +0100

    Added way to create React components

--- app/assets/javascripts/entry.jsx
+++ app/assets/javascripts/entry.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import DemoComponent from "./components/demo-component";
-import Day from "./components/day";
+require("expose?DemoComponent!./components/demo-component");
 
 require("../stylesheets/application.scss");
 require("../stylesheets/_variables.scss");
-window.onload = function() {
-  React.render(
-    <DemoComponent />,
-    document.getElementById("container")
-  );
-};
