8518fc3a07a93f22462fccaec377566bc4f5a761
andrei <andreins@ymail.com>
2015-10-10 16:46:29 +0100

    Added classes to front end

--- app/assets/javascripts/components/day.jsx
+++ app/assets/javascripts/components/day.jsx
@@ -5,11 +5,21 @@ const propTypes = {
   name: React.PropTypes.string.isRequired,
   isBig: React.PropTypes.bool.isRequired,
   containerName: React.PropTypes.string,
-  actcion:  React.PropTypes.array
+  activity1: React.PropTypes.string,
+  hour1: React.PropTypes.number,
+  activity2: React.PropTypes.string,
+  hour2: React.PropTypes.number,
+  activity3: React.PropTypes.string,
+  hour3: React.PropTypes.number,
+  activity4: React.PropTypes.string,
+  hour4: React.PropTypes.number,
 };
 
 const defaultProps = {
-};
+  activity3: " ",
+  activity4: " ",
+
+}
 
 export default class DayComponent extends React.Component {
   constructor(props) {
@@ -33,7 +43,9 @@ export default class DayComponent extends React.Component {
           <h3 className="panel-title">{this.props.name}</h3>
         </div>
         <div className="panel-body">
-          Panel content
+          <p> {this.props.activity1} - {this.props.hour1} minutes </p>
+          <p> {this.props.activity2} - {this.props.hour2} minutes </p>
+          <p> {this.props.activity3} - {this.props.hour3} minutes </p>
         </div>
       </div>
     );
