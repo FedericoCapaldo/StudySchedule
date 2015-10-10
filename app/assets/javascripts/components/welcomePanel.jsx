c3724b75db7b83313d5fdf6faf4d446d941c6842
andrei <andreins@ymail.com>
2015-10-10 12:02:26 +0100

    Added welcome information panel

--- app/assets/javascripts/components/welcomePanel.jsx
+++ app/assets/javascripts/components/welcomePanel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const displayName = "Day";
+const displayName = "welcomePanel";
 const propTypes = {
 	message: React.PropTypes.string.isRequired,
 	name: React.PropTypes.string.isRequired
@@ -19,11 +19,15 @@ export default class WelcomePanel extends React.Component {
 
 	render() {
 		return (
-			<div class="jumbotron">
+			<div className="jumbotron">
 			  <h1>Hello, { this.props.name } </h1>
 			  <p>{this.props.message}</p>
-			  <p><a class="btn btn-primary btn-lg">Learn more</a></p>
+			  <p><a className="btn btn-primary btn-lg">Take me to your leader</a></p>
 			</div>
 		);
 	}
 }
+
+WelcomePanel.displayName = displayName;
+WelcomePanel.propTypes = propTypes;
+WelcomePanel.defaultProps = defaultProps;
