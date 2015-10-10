9d22e6fe4a4a4d2c72e8efe52d91bf61e4350ea3
FedericoKCL <fed.deployment@aol.co.uk>
2015-10-10 15:23:48 +0100

    added more style

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
   	@days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  	@week = Week.find(5)
   end
 end
